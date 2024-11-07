1fda8bf6fc920be04a897a33037f0c592310e882
Jason Patel <jasonpatel24@gmail.com>
2024-11-07 14:11:04 -0500

    Initial commit
    Created a main.py file that stores stubs for all transcendental functions planned for initial release of Eternity.

---
+++ main.py
@@ -0,0 +1,31 @@
+import math # Useful math functions provided by Python
+import sys # To read user input
+
+# Inverse Cosine
+def invCos():
+    print('Under construction!')
+    return None
+
+# Logarithmic
+def log():
+    print('Under construction!')
+    return None
+
+# Mean absolute deviation
+def meanAbsDev():
+    print('Under construction!')
+    return None
+# Standard deviation
+def stdDev():
+    print('Under construction!')
+    return None
+
+# Exponentiation
+def exp():
+    print('Under construction!')
+    return None
+
+# Exponential Growth
+def expGrowth():
+    print('Under construction!')
+    return None
