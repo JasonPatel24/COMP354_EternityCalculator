d329b1a7161614ad3049280271feec6bf758e4a6
Jason Patel <jasonpatel24@gmail.com>
2024-11-11 19:57:24 -0500

    Added number validation as a helper function
    Added isNumber function, which checks if the provided value is actually a number. Uses python's numbers module.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import math # Useful math functions provided by Python
+import numbers # Useful if you need to check if your value is a real number
 import sys # To read user input
+
+def isNumber(toCheck):
+    return isinstance(toCheck, numbers.Number)
 
 # Inverse Cosine
 def invCos():
@@ -45,6 +49,10 @@ def exp(x, y):
 def expGrowth(a, b, x):
     try:
         # Error handling
+        if (not isNumber(a) or not isNumber(b) or not isNumber(x)):
+            print('ERROR: At least one of \'a\', \'b\', or \'x\' is not a valid number.')
+            exit()
+
         if (a <= 0):
             print('ERROR: Base \'a\' must be greater than 0.')
             exit()
@@ -64,4 +72,4 @@ def expGrowth(a, b, x):
 
 # FUNCTION TESTING
 print(exp(2,0.8))
-print(expGrowth(10, 0.6, -12))
+print(expGrowth(10, 0.6, 12))
